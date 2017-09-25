1340823c82b922da10a63a83f2cec379e7baba9c
sig9org <sig9@sig9.org>
2017-09-25 20:46:04 +0900

    Fix directory

--- versions/2.4.1/files/opt/semaphore/start.sh
+++ versions/2.4.1/files/opt/semaphore/start.sh
@@ -3,11 +3,11 @@
 # If this is the first time we have run this container, update place holder to env vars in config files
 if [ -e /firstrun ]; then
   echo "Starting first run actions"
-  sed -i s/MYSQL_IP_ADDR/${MYSQL_PORT_3306_TCP_ADDR}/ /opt/data/semaphore/semaphore_config.*
-  semaphore -setup < /opt/data/semaphore/semaphore_config.stdin
+  sed -i s/MYSQL_IP_ADDR/${MYSQL_PORT_3306_TCP_ADDR}/ /opt/semaphore/semaphore_config.*
+  semaphore -setup < /opt/semaphore/semaphore_config.stdin
   # Remove first run so that subsequent runs within the same container instance don't invoke first_run logic again
   rm /firstrun
 fi
 
 # Start the container
-semaphore -config /opt/data/semaphore/semaphore_config.json
+semaphore -config /opt/semaphore/semaphore_config.json

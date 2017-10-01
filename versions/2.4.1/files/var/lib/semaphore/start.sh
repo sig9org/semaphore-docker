4c9b54d40a3a5aba09a61389b656fabd0246497c
sig9org <sig9@sig9.org>
2017-10-01 23:53:52 +0900

    Fix file check script

--- versions/2.4.1/files/var/lib/semaphore/start.sh
+++ versions/2.4.1/files/var/lib/semaphore/start.sh
@@ -3,7 +3,7 @@
 /waitforit -host ${MYSQL_PORT_3306_TCP_ADDR} -port=3306 -timeout=120
 
 # If this is the first time we have run this container, update place holder to env vars in config files
-if [ -e /opt/semaphore/semaphore_config.json ]; then
+if [ ! -e /opt/semaphore/semaphore_config.json ]; then
   echo "Starting first run actions"
   sed -i s/MYSQL_IP_ADDR/mysql/ /opt/semaphore/semaphore_config.*
   semaphore -setup < /opt/semaphore/semaphore_config.stdin

8605c0a56f8bbdabc03958564ae21e1bf489fdb1
sig9org <sig9@sig9.org>
2017-10-01 23:13:45 +0900

    Change directory structure

--- versions/2.4.1/files/var/lib/semaphore/start.sh
+++ versions/2.4.1/files/var/lib/semaphore/start.sh
@@ -3,12 +3,10 @@
 /waitforit -host ${MYSQL_PORT_3306_TCP_ADDR} -port=3306 -timeout=120
 
 # If this is the first time we have run this container, update place holder to env vars in config files
-if [ -e /firstrun ]; then
+if [ -e /opt/semaphore/semaphore_config.json ]; then
   echo "Starting first run actions"
   sed -i s/MYSQL_IP_ADDR/mysql/ /opt/semaphore/semaphore_config.*
   semaphore -setup < /opt/semaphore/semaphore_config.stdin
-  # Remove first run so that subsequent runs within the same container instance don't invoke first_run logic again
-  rm /firstrun
 fi
 
 # Start the container

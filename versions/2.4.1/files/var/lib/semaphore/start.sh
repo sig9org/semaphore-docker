9d1d61fff1b4d46db37e4a029c02320a0ed21a45
sig9org <sig9@sig9.org>
2017-10-02 00:21:25 +0900

    Fix path

--- versions/2.4.1/files/var/lib/semaphore/start.sh
+++ versions/2.4.1/files/var/lib/semaphore/start.sh
@@ -5,8 +5,8 @@
 # If this is the first time we have run this container, update place holder to env vars in config files
 if [ ! -e /opt/semaphore/semaphore_config.json ]; then
   echo "Starting first run actions"
-  sed -i s/MYSQL_IP_ADDR/mysql/ /opt/semaphore/semaphore_config.*
-  semaphore -setup < /opt/semaphore/semaphore_config.stdin
+  sed -i s/MYSQL_IP_ADDR/mysql/ /var/lib/semaphore/semaphore_config.*
+  semaphore -setup < /var/lib/semaphore/semaphore_config.stdin
 fi
 
 # Start the container
